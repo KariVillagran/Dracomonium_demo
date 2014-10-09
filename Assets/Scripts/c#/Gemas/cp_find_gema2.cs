92beb8df6f52c35314d74f01ec79bcc50f492c13
KariVillagran <akarinavillagrani@gmail.com>
2014-10-08 21:52:36 -0300

    Actualizacion incluyendo paso 1 modulo gemas 08-10-2014

---
+++ Assets/Scripts/c#/Gemas/cp_find_gema2.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class cp_find_gema2 : MonoBehaviour
+{
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+}
+
